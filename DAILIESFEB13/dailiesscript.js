7e0fb2cfb783a71a37b76036116ef494fdef7204
Gabrielle Ann Franco <Gabrielle.Franco@edu.sait.ca>
2024-02-13 10:12:58 -0700

    string variables in js

---
+++ DAILIESFEB13/dailiesscript.js
@@ -0,0 +1,25 @@
+let myString = 'Draw the cat eye sharp enough to kill a man';
+myString.startsWith('Draw');
+console.log(myString.startsWith('Draw'));
+
+console.log (myString); //Output of the string will be the letter P.
+document.getElementById("heading").innerHTML = myString;
+
+myString.includes('cat');
+console.log(myString.includes('cat'));
+document.getElementById("includesStr").textContent = myString.includes('cat');
+
+myString.endsWith('man');
+console.log(myString.endsWith('man'));
+document.getElementById("endStr").textContent = myString.endsWith('man');
+
+
+myString.toLowerCase('draw');
+document.getElementById("lowerStr").textContent = myString.toLowerCase('draw');
+
+console.log(myString.toLowerCase('draw'));
+console.log(myString.trim());
+
+//let myMessage = 'x marks the spot, where we fell apart';
+
+//myMessage = myMessage.replace('x marks', 'there goes');
